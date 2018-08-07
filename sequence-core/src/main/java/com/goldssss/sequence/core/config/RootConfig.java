df76d552927da61b6da313e8256f4ccea312d605
majinxin <63212640@qq.com>
2018-08-07 22:53:40 +0800

    排除enableWebMvc

--- sequence-core/src/main/java/com/goldssss/sequence/core/config/RootConfig.java
+++ sequence-core/src/main/java/com/goldssss/sequence/core/config/RootConfig.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = "com.goldssss.sequence.core",
         excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,value = Controller.class),
-                @ComponentScan.Filter(type = FilterType.ANNOTATION,value = RestController.class)})
+                          @ComponentScan.Filter(type = FilterType.ANNOTATION,value = RestController.class),
+                          @ComponentScan.Filter(type = FilterType.ANNOTATION,value = EnableWebMvc.class)})
 public class RootConfig {
 }
